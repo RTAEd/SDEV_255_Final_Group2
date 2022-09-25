32417ada457174ee0831d3e446ca817c666a8f52
AUGuise <AU_Guise@protonmail.com>
2022-09-25 17:53:56 -0500

    Updated Main, added file for review

--- group2_project.py
+++ group2_project.py
@@ -1,6 +1,3 @@
-import random
-
-
 # Class Password():
 
 # List of lower-case elements for password
@@ -37,11 +34,11 @@ remaining_chars = list(
 
 # Request user input for definition of password
 def generate_user_input():
-    get_length = int(input("How long would you like the password to be?: "))
-    get_lower = int(input("Please enter the minimum number of lower-case letters. Enter '0' if you do not wish to include this element: "))
-    get_upper = int(input("Please enter the minimum number of upper-case letters: Enter '0' if you do not wish to include this element: "))
-    get_nums = int(input("Please enter the minimum number of numbers?: Enter '0' if you do not wish to include this element: "))
-    get_symbols = int(input("Please enter the minimum number of special symbols?: Enter '0' if you do not wish to include this element: "))
+    get_length = abs(int(input("How long would you like the password to be?: ")))
+    get_lower = abs(int(input("Please enter the minimum number of lower-case letters. Enter '0' if you do not wish to include this element: ")))
+    get_upper = abs(int(input("Please enter the minimum number of upper-case letters: Enter '0' if you do not wish to include this element: ")))
+    get_nums = abs(int(input("Please enter the minimum number of numbers?: Enter '0' if you do not wish to include this element: ")))
+    get_symbols = abs(int(input("Please enter the minimum number of special symbols?: Enter '0' if you do not wish to include this element: ")))
 
     passFormat = {'length': get_length, 'lower':get_lower, 'upper':get_upper, 'numbers':get_nums, 'symbols':get_symbols}
 
@@ -50,6 +47,7 @@ def generate_user_input():
 
 # Takes the output of the previous function as an argument and creates the password
 def passGen(passFormat):
+    import random
     password_elements = []
     
     # Fulfill the user required characters first 
@@ -78,3 +76,4 @@ print(passGen(generate_user_input()))
 
 #TODO: Convert the password generatot to a class
 #TODO: Update the generator to be able to to eliminate and element (will need to update the remainin_chars list correctly to match.)
+#TODO: Eliminate negative numbers
